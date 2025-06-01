8ccadf935e59b3ff2e2593ec6fffc5199b3fb61a
Lucas Gois <joslucasgois@gmail.com>
2025-06-01 13:10:01 -0300

    commit 01/06/2025

--- src/br/com/lucas/exceptions/RuntimeExceptions.java
+++ src/br/com/lucas/exceptions/RuntimeExceptions.java
@@ -1,5 +1,21 @@
 package br.com.lucas.exceptions;
 
 public class RuntimeExceptions {
+	
 
 }
+
+//"Throwable possui erros (graves) e exceptions (podem ser tratadas). 
+//As exceptions são divididas em checked (JRE te obriga a tratar) 
+//e unchecked (não obriga a tratar, mas podem estourar em runtime). 
+//As unchecked são filhas de RuntimeException, que é a mais genérica, 
+//e dentro dela tem classes mais específicas."
+
+//RuntimeException (Unchecked)
+//├── NullPointerException
+//├── ArrayIndexOutOfBoundsException
+//├── ArithmeticException
+//├── IllegalArgumentException
+//├── ClassCastException
+//├── NumberFormatException
+//└── ... (outras unchecked)
